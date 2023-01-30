e1e522a6cbdc60307025a5dbe6a38f42ae9551f1
Yiwei Wang <yiwei.wang@momenta.ai>
2023-01-29 22:41:58 -0500

    initial commit of complete code

---
+++ main.py
@@ -0,0 +1,32 @@
+import ast
+
+def extract_identifiers(tree):
+    identifiers = []
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Name):
+            identifiers.append(node.id)
+
+    return identifiers
+
+def max_nesting_depth(node, depth=0):
+    max_depth = depth
+    for child in ast.iter_child_nodes(node):
+        if isinstance(child, (ast.If, ast.For, ast.While, ast.FunctionDef, ast.AsyncFunctionDef)):
+            child_depth = max_nesting_depth(child, depth + 1)
+            max_depth = max(max_depth, child_depth)
+    return max_depth
+
+def main_method(file_path):
+    with open(file_path, 'r') as f:
+        tree = ast.parse(f.read())
+    identifiers = extract_identifiers(tree)
+    max_depth = max_nesting_depth(tree)
+    
+    for identifier in identifiers:
+        if len(identifier) == 13:
+            return False
+        
+    if max_depth > 4:
+        return False
+    
+    return True
---
+++ tests.py
@@ -0,0 +1,4 @@
+from main import main_method
+
+def test_true_negative():
+    assert main_method("test_cases/true_negative.py") == False
